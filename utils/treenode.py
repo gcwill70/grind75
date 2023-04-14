28ca3138f7b535fb1b48bc5d515d011902b81fd8
gcwill70 <gcwill70@gmail.com>
2023-04-14 10:39:21 -0400

    add treenode utils

---
+++ utils/treenode.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.val)
+
+    def print(self):
+        st = f"{self.val} "
+        if self.left:
+            st += self.left.print()
+        if self.right:
+            st += self.right.print()
+        return st
+
+    @staticmethod
+    def fromList(lst):
+        if not lst:
+            return None
+        root = TreeNode(lst[0])
+        queue = [root]
+        i = 1
+        while queue and i < len(lst):
+            node = queue.pop(0)
+            if lst[i] is not None:
+                node.left = TreeNode(lst[i])
+                queue.append(node.left)
+            i += 1
+            if i < len(lst) and lst[i] is not None:
+                node.right = TreeNode(lst[i])
+                queue.append(node.right)
+            i += 1
+        return root
