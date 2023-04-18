f8b2420c0682fc97460897ebf903ad077851fb7f
gcwill70 <gcwill70@gmail.com>
2023-04-18 11:01:06 -0400

    remove comments

--- max_subarray/max_subarray.py
+++ max_subarray/max_subarray.py
@@ -1,20 +1,3 @@
-# class Solution:
-#     def maxSubArray(self, nums: list[int]) -> int:
-#         return self.__maxSubArray(nums, 0, None)
-
-#     def __maxSubArray(self, nums, i, sum) -> int:
-#         print(f"__maxSubArray({nums}, {i}, {sum})")
-#         if i == len(nums):
-#             return sum
-#         # get max sum if first in sequence
-#         maximum = self.__maxSubArray(nums, i + 1, nums[i])
-#         # get max sum if middle of sequence
-#         maximum = max(maximum, self.__maxSubArray(nums, i + 1, (sum or 0) + nums[i]))
-#         # get max sum if excluded
-#         maximum = max(maximum, self.__maxSubArray(nums, i + 1, None))
-#         return maximum
-
-
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
         if len(nums) == 0:
