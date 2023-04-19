ae408795531ea927a2d37de79deb3245c7020448
gcwill70 <gcwill70@gmail.com>
2023-04-19 13:31:52 -0400

    add solution

--- longest_substring/longest_substring.py
+++ longest_substring/longest_substring.py
@@ -1,3 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        if len(s) < 2:
+            return len(s)
+        start = 0
+        end = 0
+        maxCount = 0
+        table = set()
+        while end < len(s):
+            if s[end] not in table:
+                table.add(s[end])
+                end += 1
+            else:
+                start += 1
+                end = start
+                table.clear()
+            maxCount = max(maxCount, len(table))
+        return maxCount
