acedae48fa29069d391f6885cb55beb8291d3d1b
gcwill70 <gcwill70@gmail.com>
2023-04-19 10:56:07 -0400

    solution

--- k_closest_points/k_closest_points.py
+++ k_closest_points/k_closest_points.py
@@ -1,3 +1,13 @@
+import heapq
+
+
 class Solution:
     def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
-        pass
+        heap = []
+        for x, y in points:
+            dist = -(x * x + y * y)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+        return [[x, y] for (dist, x, y) in heap]
