6dfc94a6ddeda0f4d42359b58514bdccdf223a4e
gcwill70 <gcwill70@gmail.com>
2023-04-19 10:11:42 -0400

    add template

---
+++ k_closest_points/test_k_closest_points.py
@@ -0,0 +1,13 @@
+from k_closest_points.k_closest_points import Solution
+
+
+def test1():
+    assert Solution().kClosest([[1, 3], [-2, 2]], 1) == [[-2, 2]]
+
+
+def test2():
+    assert Solution().kClosest([[3, 3], [5, -1], [-2, 4]], 2) == [[3, 3], [-2, 4]]
+
+
+def test3():
+    assert Solution().kClosest([[0, 1], [1, 0]], 2) == [[0, 1], [1, 0]]
