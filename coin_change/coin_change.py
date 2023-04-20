f919327251dbe7abaa4f3b2c03ceb3cc895eabd3
gcwill70 <gcwill70@gmail.com>
2023-04-20 12:46:21 -0400

    working without dp

--- coin_change/coin_change.py
+++ coin_change/coin_change.py
@@ -1,3 +1,14 @@
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
-        pass
+        return self.__coinChange(coins, amount, 0)
+
+    def __coinChange(self, coins, amount, count):
+        if amount < 0:
+            return -1
+        if amount == 0:
+            return count
+        counts = []
+        for coin in coins:
+            counts.append(self.__coinChange(coins, amount - coin, count + 1))
+        retVal = min([count for count in counts if count > 0] or [-1])
+        return retVal
