b2c75fdf9419572004c3252b8d1f67e51fa4ddc3
gcwill70 <gcwill70@gmail.com>
2023-04-20 13:43:45 -0400

    add bigger tests

--- coin_change/test_coin_change.py
+++ coin_change/test_coin_change.py
@@ -11,3 +11,11 @@ def test2():
 
 def test3():
     assert Solution().coinChange([1], 0) == 0
+
+
+def test4():
+    assert Solution().coinChange([1, 2, 5], 100) == 20
+
+
+def test5():
+    assert Solution().coinChange([3, 7, 405, 436], 8839) == 25
