5250e0cd523b2d25b8e4a241460931528680dfdd
gcwill70 <gcwill70@gmail.com>
2023-04-24 09:06:35 -0400

    done

--- merge_intervals/merge_intervals.py
+++ merge_intervals/merge_intervals.py
@@ -1,3 +1,15 @@
 class Solution:
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-        pass
+        # sort intervals by first
+        intervals.sort(key=lambda x: x[0])
+        # create merged interval array
+        merged = [[intervals[0][0], intervals[0][1]]]
+        i = 0
+        # insert each item, starting at earliest interval
+        for j in range(1, len(intervals)):
+            if merged[i][1] >= intervals[j][0]:
+                merged[i] = [merged[i][0], max(merged[i][1], intervals[j][1])]
+            else:
+                merged.append([intervals[j][0], intervals[j][1]])
+                i += 1
+        return merged
