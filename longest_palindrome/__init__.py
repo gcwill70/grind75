3ccbd9a2ff64123932ff89b77aef286a4d3d2b47
gcwill70 <gcwill70@gmail.com>
2023-04-27 15:16:16 -0400

    done without memoization

--- longest_palindrome/__init__.py
+++ longest_palindrome/__init__.py
@@ -1,3 +1,22 @@
+# create sliding window
+# start and end pointers
+# increment end
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        return self.__longestPalindrome(s, 0, len(s) - 1)
+
+    def __longestPalindrome(self, s: str, start: int, end: int) -> str:
+        if start > end:
+            return ""
+        if start == end:
+            return s[start]
+        # characters are equal, advance inward
+        if s[start] == s[end]:
+            return s[start] + self.__longestPalindrome(s, start + 1, end - 1) + s[end]
+        # characters arent equal, try both directions
+        first = self.__longestPalindrome(s, start + 1, end)
+        second = self.__longestPalindrome(s, start, end - 1)
+        # return longest from either direction
+        return first if len(first) >= len(second) else second
--- longest_palindrome/test_longest_palindrome.py
+++ longest_palindrome/test_longest_palindrome.py
@@ -2,8 +2,8 @@ from longest_palindrome import Solution
 
 
 def test1():
-    assert Solution().longestPalindrome("babad") == "bab"
+    assert Solution().longestPalindrome("babad") in ["aba", "bab"]
 
 
 def test2():
-    assert Solution().longestPalindrome("cbbd") == "bb"
+    assert Solution().longestPalindrome("cbbd") in ["bb"]
