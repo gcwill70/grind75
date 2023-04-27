220728a688e21b1ab1a980627ccbd1a8cbcc83b2
gcwill70 <gcwill70@gmail.com>
2023-04-27 13:33:37 -0400

    oops

--- longest_palindrome/test_longest_palindrome.py
+++ longest_palindrome/test_longest_palindrome.py
@@ -1,3 +1,9 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        pass
+from longest_palindrome import Solution
+
+
+def test1():
+    assert Solution().longestPalindrome("babad") == "bab"
+
+
+def test2():
+    assert Solution().longestPalindrome("cbbd") == "bb"
