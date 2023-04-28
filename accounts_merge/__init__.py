c8be3f02bd910a4100cca0dad0e7c8cdffc8a8eb
gcwill70 <gcwill70@gmail.com>
2023-04-28 15:47:19 -0400

    wont merge if emails are chained

--- accounts_merge/__init__.py
+++ accounts_merge/__init__.py
@@ -9,29 +9,38 @@
 # collect entries with the same key (name) and merge email lists and sort - O(N*MlogM)
 
 # solution 3:
-# merged = [accounts[0]]
-# for each
+# create hash table of names and emails {'name': set('email1', 'email2', ...)}
+# for each account, if name and email is shared, merged lists - O(M)
+# at the end, sort email lists
 
 
 class Solution:
     def accountsMerge(self, accounts: list[list[str]]) -> list[list[str]]:
-        # sort list by name
-        accounts.sort(key=lambda account: account[0])
-        merged = [accounts[0]]
-        i = 1
-        while i < len(accounts):  # iterate through accounts
-            j = i
-            # find all duplicates
-            while j < len(accounts) and accounts[j][0] == accounts[i][0]:
-                j += 1
-            # create sorted list of all emails
-            emails = []
-            for k in range(i, j):
-                for email in accounts[k][1:]:
-                    if email not in emails:
-                        emails.append(email)
-            # add sorted list to merged
-            merged.append([accounts[i][0]] + emails)
-            # move to next non-duplicate
-            i = j
-        return merged
+        # create initial database
+        db: dict[str, list[set[str]]] = {accounts[0][0]: [set(accounts[0][1:])]}
+        # add accounts to db
+        for i in range(1, len(accounts)):
+            name = accounts[i][0]
+            emails = set(accounts[i][1:])
+            if name in db:
+                # check for merge of existing email set
+                merged = False
+                for j in range(0, len(db[name])):  # for each set of emails
+                    if db[name][j] & emails:
+                        db[name][j] = db[name][j] | emails
+                        merged = True
+                        break
+                # if not merged, append to end
+                if not merged:
+                    db[name].append(emails)
+            else:
+                db[name] = [emails]
+        # merge existing entries
+        # convert map to list format
+        ret = []
+        for name, sets in db.items():
+            for eset in sets:
+                eset = list(eset)
+                eset.sort(key=lambda x: x)
+                ret.append([name] + eset)
+        return ret
--- accounts_merge/test_accounts_merge.py
+++ accounts_merge/test_accounts_merge.py
@@ -2,26 +2,26 @@ from accounts_merge import Solution
 
 
 def test1():
-    assert set(
-        Solution().accountsMerge(
-            [
-                ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
-                ["John", "johnsmith@mail.com", "john00@mail.com"],
-                ["Mary", "mary@mail.com"],
-                ["John", "johnnybravo@mail.com"],
-            ]
-        )
-    ) == set(
+    accounts = Solution().accountsMerge(
         [
-            ["John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"],
+            ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+            ["John", "johnsmith@mail.com", "john00@mail.com"],
             ["Mary", "mary@mail.com"],
             ["John", "johnnybravo@mail.com"],
         ]
     )
+    assert (
+        accounts.index(
+            ["John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"]
+        )
+        != -1
+    )
+    assert accounts.index(["Mary", "mary@mail.com"]) != -1
+    assert accounts.index(["John", "johnnybravo@mail.com"]) != -1
 
 
 def test2():
-    assert Solution().accountsMerge(
+    accounts = Solution().accountsMerge(
         [
             ["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"],
             ["Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"],
@@ -29,10 +29,40 @@ def test2():
             ["Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"],
             ["Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"],
         ]
-    ) == [
-        ["Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"],
-        ["Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"],
-        ["Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"],
-        ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"],
-        ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"],
-    ]
+    )
+    assert accounts.index(["Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"]) != -1
+    assert (
+        accounts.index(
+            ["Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"],
+        )
+        != -1
+    )
+    assert accounts.index(["Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"]) != -1
+    assert accounts.index(["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"]) != -1
+    assert accounts.index(["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"]) != -1
+
+
+def test3():
+    accounts = Solution().accountsMerge(
+        [
+            ["David", "David0@m.co", "David1@m.co"],
+            ["David", "David3@m.co", "David4@m.co"],
+            ["David", "David4@m.co", "David5@m.co"],
+            ["David", "David2@m.co", "David3@m.co"],
+            ["David", "David1@m.co", "David2@m.co"],
+        ]
+    )
+    assert (
+        accounts.index(
+            [
+                "David",
+                "David0@m.co",
+                "David1@m.co",
+                "David2@m.co",
+                "David3@m.co",
+                "David4@m.co",
+                "David5@m.co",
+            ]
+        )
+        != -1
+    )
