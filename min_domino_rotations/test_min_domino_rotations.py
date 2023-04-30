d37b13b1a9a3a5b21bcda1904843f8a752eabfa8
gcwill70 <gcwill70@gmail.com>
2023-04-30 12:41:13 -0400

    add min domino rotation

---
+++ min_domino_rotations/test_min_domino_rotations.py
@@ -0,0 +1,9 @@
+from min_domino_rotations import Solution
+
+
+def test1():
+    assert Solution().minDominoRotations([2, 1, 2, 4, 2, 2], [5, 2, 6, 2, 3, 2]) == 2
+
+
+def test2():
+    assert Solution().minDominoRotations([3, 5, 1, 2, 3], [3, 6, 3, 3, 4]) == -1
