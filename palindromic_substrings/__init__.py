0558de534567d9ba2efbedf2802409bf0f80941c
gcwill70 <gcwill70@gmail.com>
2023-05-01 18:22:09 -0400

    with memoization

--- palindromic_substrings/__init__.py
+++ palindromic_substrings/__init__.py
@@ -1,12 +1,18 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         ct = 0
+        memo: set = {}
         for i in range(len(s)):
-            ct += self.__countSubstrings(s, i, i)
-            ct += self.__countSubstrings(s, i, i + 1)
+            ct += self.__countSubstrings(s, i, i, memo)
+            ct += self.__countSubstrings(s, i, i + 1, memo)
         return ct
 
-    def __countSubstrings(self, s: str, left, right):
-        if left < 0 or right >= len(s) or s[left] != s[right]:
-            return 0
-        return 1 + self.__countSubstrings(s, left - 1, right + 1)
+    def __countSubstrings(self, s: str, left, right, memo: set):
+        if (left, right) not in memo:
+            if left < 0 or right >= len(s) or s[left] != s[right]:
+                memo[(left, right)] = 0
+            else:
+                memo[(left, right)] = 1 + self.__countSubstrings(
+                    s, left - 1, right + 1, memo
+                )
+        return memo[(left, right)]
