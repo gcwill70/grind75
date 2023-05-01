c020279511d757186827e081a5c17c19341a42fd
gcwill70 <gcwill70@gmail.com>
2023-05-01 18:16:51 -0400

    without memoization

--- palindromic_substrings/__init__.py
+++ palindromic_substrings/__init__.py
@@ -1,3 +1,12 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        pass
+        ct = 0
+        for i in range(len(s)):
+            ct += self.__countSubstrings(s, i, i)
+            ct += self.__countSubstrings(s, i, i + 1)
+        return ct
+
+    def __countSubstrings(self, s: str, left, right):
+        if left < 0 or right >= len(s) or s[left] != s[right]:
+            return 0
+        return 1 + self.__countSubstrings(s, left - 1, right + 1)
